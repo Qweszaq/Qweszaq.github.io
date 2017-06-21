49f964bcbe155ef7734f15c362163a0c518bd07d
Qweszaq <qweszaqmichalb1@gmail.com>
2017-06-22 00:38:03 +0200

    Basic, no translation yet

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿function swapp()
+{
+	if(document.getElementById('main').classList == 'main1')
+	{
+		document.getElementById('main').classList.remove('main1');
+		document.getElementById('main').classList.add('main2');
+	}
+	else
+	{
+		document.getElementById('main').classList.remove('main2');
+		document.getElementById('main').classList.add('main1');
+	}
+}
